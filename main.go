27bca6d81a4865f2c2cfb978f32cceef57b3cdd9
mpetavy <mpetavy>
2023-02-05 00:11:14 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"github.com/glebarez/sqlite"
+	"github.com/mpetavy/common"
+	"gorm.io/gorm"
+)
+
+func init() {
+	common.Init(false, "0.0.0", "", "", "2018", "test", "mpetavy", fmt.Sprintf("https://github.com/mpetavy/%s", common.Title()), common.APACHE, nil, nil, nil, run, 0)
+}
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
+func run() error {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	common.Error(db.AutoMigrate(&Product{}))
+
+	// Create
+	db.Create(&Product{Code: "D42", Price: 100})
+
+	// Read
+	var product Product
+	db.First(&product, 1)                 // find product with integer primary key
+	db.First(&product, "code = ?", "D42") // find product with code D42
+
+	// Update - update product's price to 200
+	db.Model(&product).Update("Price", 200)
+	// Update - update multiple fields
+	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
+	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+
+	// Delete - delete product
+	//db.Delete(&product, 1)
+
+	return nil
+}
+
+func main() {
+	defer common.Done()
+
+	common.Run(nil)
+}
